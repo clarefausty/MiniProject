ae080e2a47ba35deeb658fa3fe694bbb886e488e
clarefausty <clarefausty@gmail.com>
2023-11-29 00:26:04 +0100

    first commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log("I am moody and joyous");
+console.log("I am happy and calm");
+console.log("I am happy and lovable");
+const btn = document.getElementById("btn");
+btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", function () {
+    alert("clicked!!!");
+});
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+console.log("I am moody and joyous")
+console.log("I am happy and calm")
+console.log("I am happy and lovable")
+
+const btn = document.getElementById("btn")
+btn?.addEventListener("click", function(){
+    alert("clicked!!!")
+})
