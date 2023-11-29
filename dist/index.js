9f27c3aab5d3bc75166e1a7e49514aaaa98b676f
clarefausty <clarefausty@gmail.com>
2023-11-29 14:27:13 +0100

    Created to-do list

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,17 @@
 "use strict";
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoinput");
-btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", function () {
-    alert(input.value);
-    input.value;
-});
+const form = document.querySelector("form");
+const list = document.getElementById("todolist");
+function handleSubmit(e) {
+    e.preventDefault();
+    const newToDo = input.value;
+    const newLI = document.createElement("li");
+    newLI.append(newToDo);
+    list === null || list === void 0 ? void 0 : list.append(newLI);
+}
+form === null || form === void 0 ? void 0 : form.addEventListener("submit", handleSubmit);
+// btn?.addEventListener("click", function(){
+//     alert(input.value)
+//     input.value
+// })
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 
 
-const btn = document.getElementById("btn")
+const btn = document.getElementById("btn") as HTMLButtonElement
 const input = document.getElementById("todoinput") as HTMLInputElement
+const form = document.querySelector("form")
+const list = document.getElementById("todolist")
+
+function handleSubmit(e : SubmitEvent){
+    e.preventDefault()
+    const newToDo = input.value
+    const newLI = document.createElement("li")
+    newLI.append(newToDo)
+    list?.append(newLI)
+    
+}
 
 
-btn?.addEventListener("click", function(){
-    alert(input.value)
-    input.value
-})
+form?.addEventListener("submit", handleSubmit)
+
+
+// btn?.addEventListener("click", function(){
+//     alert(input.value)
+//     input.value
+// })
