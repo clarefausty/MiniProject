0457dd51d2432f296940c528eb359cf0bf9b579e
clarefausty <clarefausty@gmail.com>
2023-11-29 10:17:39 +0100

    input addition

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,7 @@
 "use strict";
-console.log("I am moody and joyous");
-console.log("I am happy and calm");
-console.log("I am happy and lovable");
 const btn = document.getElementById("btn");
+const input = document.getElementById("todoinput");
 btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", function () {
-    alert("clicked!!!");
+    alert(input.value);
+    input.value;
 });
--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ const input = document.getElementById("todoinput") as HTMLInputElement
 
 
 btn?.addEventListener("click", function(){
+    alert(input.value)
     input.value
 })
